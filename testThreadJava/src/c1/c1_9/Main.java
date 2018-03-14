f67944e7d622dc296b6d30047b7b9eaaa01da41d
Administrator <Administrator@PC-201705081149>
2018-03-15 02:15:08 +0800

    提交1.9章节，线程中的异常

---
+++ testThreadJava/src/c1/c1_9/Main.java
@@ -0,0 +1,13 @@
+package c1.c1_9;
+
+// 现在，实现例子主类。实现 Main类和 main() 方法.
+public class Main {
+	
+	public static void main(String[] args) {
+		// 创建 Task 对象并用线程运行它。使用 setUncaughtExceptionHandler() 方法设置非检查异常 handler 并开始执行线程。
+		Task task = new Task();
+		Thread thread = new Thread(task);
+		thread.setUncaughtExceptionHandler(new ExceptionHandler());
+		thread.start();
+	}
+}
